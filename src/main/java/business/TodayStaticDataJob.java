52680021101a0c180a12822ad6f94a5623e91a59
景育 <1245909607@qq.com>
2025-07-26 17:52:51 +0800

    feat: Hantang Daily 每天录入 static 表时，只保留 type id 在白名单中，并且标题和关键词不包含黑名单关键词的视频。

--- src/main/java/business/TodayStaticDataJob.java
+++ src/main/java/business/TodayStaticDataJob.java
@@ -18,6 +18,7 @@ import java.util.*;
 public class TodayStaticDataJob {
     private final Set<Long> aidSet;
     private final List<VideoStaticDO> allVideoStaticDOList;
+    private static final StaticDataFilter STATIC_DATA_FILTER;
     private static final String[] KEYWORDS;
     private static final int TIME_RANGE;
     private static final int PAGE_SIZE;
@@ -40,7 +41,7 @@ public class TodayStaticDataJob {
             TIME_RANGE = Integer.parseInt(properties.getProperty("static.time_range", "86400"));
             PAGE_SIZE = Integer.parseInt(properties.getProperty("static.page_size", "50"));
             KEYWORDS = properties.getProperty("static.keywords", "洛天依,言和,乐正绫").replace("\n", "").split(",");
-
+            STATIC_DATA_FILTER = new StaticDataFilter(properties);
         } catch (IOException e) {
             e.fillInStackTrace();
             throw new RuntimeException("无法加载数据库配置文件", e);
@@ -58,6 +59,8 @@ public class TodayStaticDataJob {
         for (String keyword : KEYWORDS) {
             boolean isPubdateInLimit = true;
             int pageTotal = 1000 / PAGE_SIZE;
+            // 测试环境下
+            pageTotal = 1;
             for (int page = 1; page <= pageTotal && isPubdateInLimit; page++) {
                 logger.debug("Getting static data (search results) from Bilibili API. keyword: {}, page: {}.", keyword, page);
                 List<VideoStaticDO> curVideoStaticDOList = bilibiliApi.getSearchResult(keyword, page, PAGE_SIZE);
@@ -70,7 +73,7 @@ public class TodayStaticDataJob {
 
                     // 根据AV号哈希去重。比如一首洛天依、乐正绫合唱的歌，会被搜到两次。
                     long aid = videoStaticDO.aid();
-                    if (!aidSet.contains(aid)) {
+                    if (!aidSet.contains(aid) && STATIC_DATA_FILTER.shouldRetain(videoStaticDO)) {
                         aidSet.add(aid);
                         allVideoStaticDOList.add(videoStaticDO);
                     }
